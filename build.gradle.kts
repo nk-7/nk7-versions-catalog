966999c557482cb39fc7553b0b46f1772abafe1a
Nikolay Kuleshov <nakuleshov@gmail.com>
2024-05-08 16:39:48 +0300

    Add logging dependecies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,25 @@ group = "dev.nk7"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    maven(uri(properties["repo.releases.url"]!!))
 }
 publishing {
+    repositories {
+        maven {
+            url = uri(
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    project.properties["repo.snapshots.url"].toString()
+                } else {
+                    project.properties["repo.releases.url"].toString()
+                }
+            )
+            credentials {
+                username = project.properties["repo.username"].toString()
+                password = project.properties["repo.password"].toString()
+            }
+        }
+
+    }
     publications {
         create<MavenPublication>("nk7-platform") {
             from(components["javaPlatform"])
@@ -23,20 +39,23 @@ javaPlatform {
 
 dependencies {
     api(platform(libs.kotlin))
+
     api(platform(libs.spring.boot))
     api(platform(libs.spring.cloud))
     api(platform(libs.spring.statemachine))
+
     api(platform(libs.ktor))
     api(platform(libs.exposed))
     api(platform(libs.junit))
 
     api(libs.hikari)
     api(libs.mysql)
+    api(libs.postgresql)
     api(libs.sqlite)
     api(libs.liquibase)
     api(libs.kafka.clients)
     api(libs.kafka.stream)
-    api(libs.postgresql)
-
+    api(libs.slf4j)
+    api(libs.logback.classic)
     api(libs.assertj)
 }
