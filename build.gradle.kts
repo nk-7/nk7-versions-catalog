a9f77a53469e5b5876c1d67b1bfa7ddfc5f6815b
Nikolay Kuleshov <nakuleshov@gmail.com>
2024-05-08 12:26:36 +0300

    Init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,42 @@
 plugins {
-    id("java")
+    `java-platform`
+    `maven-publish`
 }
 
 group = "dev.nk7"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+publishing {
+    publications {
+        create<MavenPublication>("nk7-platform") {
+            from(components["javaPlatform"])
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    api(platform(libs.kotlin))
+    api(platform(libs.spring.boot))
+    api(platform(libs.spring.cloud))
+    api(platform(libs.spring.statemachine))
+    api(platform(libs.ktor))
+    api(platform(libs.exposed))
+    api(platform(libs.junit))
+
+    api(libs.hikari)
+    api(libs.mysql)
+    api(libs.sqlite)
+    api(libs.liquibase)
+    api(libs.kafka.clients)
+    api(libs.kafka.stream)
+    api(libs.postgresql)
+
+    api(libs.assertj)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "nk7-platform"
+dependencyResolutionManagement{
+    versionCatalogs{
+        create("libs"){
+            from(files("libs.versions.toml"))
+        }
+    }
+}
 
